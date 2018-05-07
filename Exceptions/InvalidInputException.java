5c98e2b0e5ac97284c96325a0255460a25727f21
Mani9723 <38226555+Mani9723@users.noreply.github.com>
2018-05-07 12:37:25 -0400

    Add files via upload

---
+++ Exceptions/InvalidInputException.java
@@ -0,0 +1,14 @@
+package RevisedBigInt.Exceptions;
+
+public class InvalidInputException extends Exception
+{
+
+	public InvalidInputException()
+	{
+		super();
+	}
+	public InvalidInputException(String message)
+	{
+		super(message);
+	}
+}

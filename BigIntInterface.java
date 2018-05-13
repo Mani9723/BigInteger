4bbcbc0acc267f42191a2ef959c6ca2896297f95
Mani9723 <38226555+Mani9723@users.noreply.github.com>
2018-05-12 21:15:19 -0400

    Add files via upload

---
+++ BigIntInterface.java
@@ -0,0 +1,11 @@
+package RevisedBigInt;
+
+public interface BigIntInterface
+{
+
+	BigInt add(BigInt other);
+	BigInt subtract(BigInt other);
+	BigInt multiply(BigInt other);
+	BigInt divideBy(BigInt other);
+
+}

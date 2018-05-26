a5e3b543e4c07ef080d2623a37acf9f261c788c6
Mani9723 <38226555+Mani9723@users.noreply.github.com>
2018-05-26 12:15:52 -0400

    Add files via upload

--- BigIntInterface.java
+++ BigIntInterface.java
@@ -2,10 +2,39 @@ package RevisedBigInt;
 
 public interface BigIntInterface
 {
-
+	/**
+	 * Calculates a+b = c, where {a,b,c | a E Z, b E Z, c E Z}
+	 * @param other - BigInt
+	 * @return this + other
+	 */
 	BigInt add(BigInt other);
+
+	/**
+	 Calculates a-b = c, where {a,b,c | a E Z, b E Z, c E Z}
+	 * @param other - BigInt
+	 * @return this - other
+	 */
 	BigInt subtract(BigInt other);
+
+	/**
+	 Calculates a*b = c, where {a,b,c | a E Z, b E Z, c E Z}
+	 * @param other - BigInt
+	 * @return this * other
+	 */
 	BigInt multiply(BigInt other);
+
+	/**
+	 Calculates a/b = c, where {a,b,c | a E Z, b E Z, c E Z}
+	 * @param other - BigInt
+	 * @return this / other
+	 */
 	BigInt divideBy(BigInt other);
+
+	/**
+	 Calculates a%b = c, where {a,b,c | a E Z, b E Z, c E Z}
+	 * @param other - BigInt
+	 * @return this % other
+	 */
+	BigInt mod(BigInt other);
 
 }
